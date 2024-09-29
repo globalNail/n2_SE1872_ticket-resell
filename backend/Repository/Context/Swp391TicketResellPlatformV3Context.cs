1b01b64245d1919272d52d6490caec9ed465de4f
globalNail <nthnamto147@gmail.com>
2024-09-29 19:41:20 +0700

    Update .gitignore to exclude unnecessary files and folders

--- backend/Repository/Context/Swp391TicketResellPlatformV3Context.cs
+++ backend/Repository/Context/Swp391TicketResellPlatformV3Context.cs
@@ -1,17 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Repository.Models;
 
-namespace Repository;
+namespace Repository.Context;
 
-public partial class Swp391ticketResellPlatformV3Context : DbContext
+public partial class Swp391TicketResellPlatformV3Context : DbContext
 {
-    public Swp391ticketResellPlatformV3Context()
+    public Swp391TicketResellPlatformV3Context()
     {
     }
 
-    public Swp391ticketResellPlatformV3Context(DbContextOptions<Swp391ticketResellPlatformV3Context> options)
+    public Swp391TicketResellPlatformV3Context(DbContextOptions<Swp391TicketResellPlatformV3Context> options)
         : base(options)
     {
     }
@@ -38,9 +37,19 @@ public partial class Swp391ticketResellPlatformV3Context : DbContext
 
     public virtual DbSet<Wallet> Wallets { get; set; }
 
+    public static string GetConnectionString(string connectionStringName)
+    {
+        var config = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        string connectionString = config.GetConnectionString(connectionStringName);
+        return connectionString;
+    }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=(local);Database=SWP391TicketResellPlatformV3;UID=sa;PWD=12345;TrustServerCertificate=True");
+        => optionsBuilder.UseSqlServer(GetConnectionString("DefaultConnection"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
