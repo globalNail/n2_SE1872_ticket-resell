0a80ae4ec88c6aeeb061a2bba6efa000dfa2fcdb
Khanh Tien <nthnamto147@gmail.com>
2024-10-22 19:25:43 +0700

    cc

--- frontend/src/pages/tickets/ViewTicketPage.jsx
+++ frontend/src/pages/tickets/ViewTicketPage.jsx
@@ -1,4 +1,3 @@
-// src/pages/Tickets/ViewTicketPage.jsx
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import ticketApi from "../../api/ticketApi";
